2eab98dc1c025e798fc1144295161acd3dbb1890
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-05 20:59:34 +0530

    Initial Commit

---
+++ Comic App/Comic_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Comic_AppApp.swift
+//  Comic App
+//
+//  Created by Mahesh Prasad on 05/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Comic_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
